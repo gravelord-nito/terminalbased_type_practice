c2241d04e493e5572cf1ed62ef55bcb638bb4cc6
gravelord-nito <godeater2579@gmail.com>
2023-01-18 11:54:37 +0330

    Committed template files

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "helper_windows.h"
+#include "colorize.h"
+
+void my_callback_on_key_arrival(char c);
+
+int main(){
+    HANDLE thread_id = start_listening(my_callback_on_key_arrival);
+    
+    WaitForSingleObject(thread_id,INFINITE);
+    return 0;
+}
+
+void my_callback_on_key_arrival(char c){
+
+}
